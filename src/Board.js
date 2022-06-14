37024b8d0cca3bc714c9b19fa358ccc29a42fe33
Travis You <1244098000@qq.com>
2022-06-14 17:50:23 +0800

    feat(Board): add Board

---
+++ src/Board.js
@@ -0,0 +1,14 @@
+import './Board.css';
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    render() {
+        return ();
+    }
+}
