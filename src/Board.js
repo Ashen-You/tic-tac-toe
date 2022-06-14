2ea9f3f0c02dd38ba4f2548faa928dd6083cfc50
Travis You <1244098000@qq.com>
2022-06-14 18:32:29 +0800

    feat: update Square and Board to finish click function

--- src/Board.js
+++ src/Board.js
@@ -7,13 +7,24 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
 
     renderSquare(i) {
         return(
             <Square
                 value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
             />
         )
     }
